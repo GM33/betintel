cf4ff0266999135509a0b03204002cc537c40f23
GM33 <168594601+GM33@users.noreply.github.com>
2026-06-04 01:27:09 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-export DATABASE_URL="postgresql://postgres:cgktPPerQvmdJMyAuYcMAxkUsqoniycZ@postgres.railway.internal:5432/railway"
-python -m mlb.startup
-uvicorn mlb.api.main:app --host 0.0.0.0 --port $PORT
