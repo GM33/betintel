88472c6c11577beea9b4c77e6502494bcc0a27f7
GM33 <168594601+GM33@users.noreply.github.com>
2026-06-04 01:32:42 -0500

    Change uvicorn command to exec for better handling

--- mlb/start.sh
+++ mlb/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 export DATABASE_URL="postgresql://postgres:cgktPPerQvmdJMyAuYcMAxkUsqoniycZ@postgres.railway.internal:5432/railway"
+cd /app/mlb
 python -m mlb.startup
-uvicorn mlb.api.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn mlb.api.main:app --host 0.0.0.0 --port $PORT
